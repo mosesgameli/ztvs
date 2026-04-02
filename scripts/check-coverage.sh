a576462736a5ca0f2260a5ce769868b6548da0d9
mosesgameli <mosesgameli20@gmail.com>
2026-04-02 08:55:01 +0000

    fix(coverage): update coverage script to use aggregate calculation

--- scripts/check-coverage.sh
+++ scripts/check-coverage.sh
@@ -3,7 +3,8 @@ set -e
 
 # Run tests and generate coverage profile
 echo "Running unit tests and generating coverage profile..."
-go test -v -coverpkg=./cmd/...,./internal/...,./pkg/... -coverprofile=coverage.out ./cmd/... ./internal/... ./pkg/...
+# Exclude /test/ directory and use all packages
+go test -count=1 -coverprofile=coverage.out ./...
 
 # Extract total coverage percentage
 COVERAGE=$(go tool cover -func=coverage.out | grep total | grep -Eo '[0-9]+\.[0-9]+')
